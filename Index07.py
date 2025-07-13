c4bf341db1d9013f5e1498ba0d26d72949f96e8e
ogiloy <ogiloykurbanyeva@gmail.com>
2025-07-13 13:17:31 +0500

    ishlandi

--- Index07.py
+++ Index07.py
@@ -6,4 +6,10 @@ def main(s,n):
     Returns:
         str: answer
     """
-    return
+    if 0 <= n < len(s):
+        return s[n]
+    else:
+        return False
+    print(main("hello", 1)) #output : 'e'
+    print(main("hello" , 5)) # output : false
+    print(main("hi", -1))
